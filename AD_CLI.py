a4028c46c73fe5f5013c1e4d85e9b994194a1403
quinton <dropatuningmetal@gmail.com>
2022-03-18 19:40:09 -0700

    string formatter and file writer now implemented

--- AD_CLI.py
+++ AD_CLI.py
@@ -1,4 +1,36 @@
-import optparse
+import argparse
+from DesktopEntryStruct import DesktopEntry
+
 
 def main():
-    pass
+    # set up argument parser
+    parser = argparse.ArgumentParser(prog='AD_CLI', description='Automatically create and install .desktop files for '
+                                                                'your apps', )
+
+    parser.add_argument('name', type=str, help='The name of the app')
+    parser.add_argument('exe_path', type=str, help='Path to the app\'s executable')
+    parser.add_argument('-d', '--description', type=str, default="", help="Application description")
+    parser.add_argument('-i', '--icon_path', type=str, help='Path to an icon image for the app')
+    parser.add_argument('-t', action='store_true', help='Execute with external terminal')
+    parser.add_argument('--entry_type', type=str, default='Application', help='The type of entry (default: Application')
+
+    # parse the args
+    args = parser.parse_args()
+
+    # create object with inputted data
+    new_entry = DesktopEntry(args.exe_path,
+                             args.name,
+                             args.icon_path,
+                             args.entry_type,
+                             args.t,
+                             args.description)
+
+    # flush that data into a file in the proper location
+    if not new_entry.is_empty():
+        print(args)
+        print(new_entry.to_str())
+        new_entry.flush()
+
+
+if __name__ == '__main__':
+    main()
--- DesktopEntryStruct.py
+++ DesktopEntryStruct.py
@@ -1,5 +1,4 @@
-from os.path import join
-from os.path import isfile
+from os.path import join, isfile, expanduser
 
 
 class DesktopEntry:
@@ -7,36 +6,42 @@ class DesktopEntry:
     ico_path: str
     name: str
     exe_path: str
-    version: int
-    encoding: str
     terminal: bool
+    description: str
 
-    def __init__(self, encoding: str, version: int, exe_path: str, name: str, ico_path: str, entry_type="Application",
-                 terminal=False):
+    def __init__(self, exe_path: str, name: str, ico_path: str, entry_type: str,
+                 terminal: bool, description: str):
         self.terminal = terminal
         self.entry_type = entry_type
         self.ico_path = ico_path
         self.name = name
         self.exe_path = exe_path
-        self.version = version
-        self.encoding = encoding
+        self.description = description
 
     def to_str(self):
-        formatted_string = f"[Desktop Entry]\n\
-                            Encoding={self.encoding}\n\
-                            Version={self.version}\n\
-                            Type={self.entry_type}\n\
-                            Terminal={self.terminal}\n\
-                            Exec={self.exe_path}\n\
-                            Name={self.name}\n\
-                            Icon={self.ico_path}\n"
+        return f'[Desktop Entry]\n' \
+               f'Name={self.name}\n' \
+               f'Type={self.entry_type}\n' \
+               f'Terminal={str(self.terminal).lower()}\n' \
+               f'Exec={self.exe_path}\n' \
+               f'Icon={self.ico_path}\n' \
+               f'Comment={self.description}'
 
     def gen_filename(self):
         return self.name.join(self.name.split()).lower() + ".desktop"
 
     # print desktop entry to file
-    def flush(self, entry_dir="~/.local/share/applications/"):
+    def flush(self, entry_dir=f"{expanduser('~')}/.local/share/applications/"):
         newfile = join(entry_dir, self.gen_filename())
         if not isfile(newfile):
             with open(newfile, 'w') as entry_file:
                 entry_file.write(self.to_str())
+
+    # validate object is not null-populated
+    def is_empty(self) -> bool:
+        return None in (self.terminal,
+                        self.entry_type,
+                        self.ico_path,
+                        self.name,
+                        self.exe_path,
+                        self.description)
